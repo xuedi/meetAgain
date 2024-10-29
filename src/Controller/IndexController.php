f15ba676b7e7e2f6502c99cacb9118176c379027
Daniel Koch <xuedi@beijingcode.org>
2024-10-29 18:15:14 +0100

    prepped up homepage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(EventRepository $repo): Response
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'events' => $repo->getUpcomingEvents(),
         ]);
     }
 }
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Event;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,18 @@ class EventRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Event::class);
+    }
+
+    public function getUpcomingEvents(int $number = 3): array
+    {
+        $query = $this->getEntityManager()->createQuery(
+            'SELECT e
+            FROM App\Entity\Event e
+            WHERE e.start > :date
+            ORDER BY e.start ASC'
+        )->setParameter('date', new DateTime());
+
+        return $query->getResult();
     }
 
     //    /**
