45c1838dec7ef73f5e671b52e17bd948aa80cbd0
Daniel Koch <xuedi@beijingcode.org>
2024-11-26 11:52:15 +0100

    removed manual logging

--- src/Controller/Admin/AdminTranslationController.php
+++ src/Controller/Admin/AdminTranslationController.php
@@ -2,34 +2,11 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Event;
-use App\Entity\Host;
-use App\Entity\Image;
-use App\Entity\Location;
-use App\Entity\User;
-use App\Form\EventType;
-use App\Form\HostType;
-use App\Form\LocationType;
-use App\Form\UserType;
-use App\Repository\CmsRepository;
-use App\Repository\ConfigRepository;
-use App\Repository\EventRepository;
-use App\Repository\HostRepository;
-use App\Repository\LocationRepository;
-use App\Repository\UserRepository;
-use App\Service\EventService;
 use App\Service\TranslationService;
-use App\Service\UploadService;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Asset\Packages as AssertManager;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Throwable;
 
 #[Route('/admin/translations')]
 class AdminTranslationController extends AbstractController
@@ -53,28 +30,18 @@ class AdminTranslationController extends AbstractController
     }
 
     #[Route('/extract', name: 'app_admin_translation_extract')]
-    public function translationsExtract(TranslationService $translationService, LoggerInterface $logger): Response
+    public function translationsExtract(TranslationService $translationService): Response
     {
-        try {
-            return $this->render('admin/translations/extract.html.twig', [
-                'result' => $translationService->extract(),
-            ]);
-        } catch (Throwable $exception) {
-            $logger->info('translationsPublish(): I got an error: ' . $exception->getMessage());
-            return $this->redirectToRoute('app_admin_translation');
-        }
+        return $this->render('admin/translations/extract.html.twig', [
+            'result' => $translationService->extract(),
+        ]);
     }
 
     #[Route('/publish', name: 'app_admin_translation_publish')]
-    public function translationsPublish(TranslationService $translationService, LoggerInterface $logger): Response
+    public function translationsPublish(TranslationService $translationService): Response
     {
-        try {
-            return $this->render('admin/translations/publish.html.twig', [
-                'result' => $translationService->publish(),
-            ]);
-        } catch (Throwable $exception) {
-            $logger->info('translationsPublish(): I got an error: ' . $exception->getMessage());
-            return $this->redirectToRoute('app_admin_translation');
-        }
+        return $this->render('admin/translations/publish.html.twig', [
+            'result' => $translationService->publish(),
+        ]);
     }
 }
