1264a4390cc90389386947c6aeb2bb2570e4741c
Daniel Koch <xuedi@beijingcode.org>
2024-12-10 14:25:55 +0100

    make username changeable in profile

--- src/Twig/AssetExistsExtension.php
+++ src/Twig/AssetExistsExtension.php
@@ -1,7 +1,6 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Twig;
-
 
 use Symfony\Component\HttpKernel\KernelInterface;
 use Twig\Extension\AbstractExtension;
---
+++ src/Twig/FormatBytesExtension.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class FormatBytesExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFilter('format_bytes', $this->formatBytes(...)),
+        ];
+    }
+
+    public function formatBytes($bytes): string
+    {
+        $units = ['B', 'KiB', 'MiB', 'GiB', 'TiB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        $bytes /= 1024 ** $pow;
+
+        return round($bytes, 2) . ' ' . $units[$pow];
+    }
+}
